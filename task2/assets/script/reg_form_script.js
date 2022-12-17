c119788dc7b0f2a2e8165c0ddb3f69ff93823bd0
Vlada <lartwoods@gmail.com>
2022-12-18 01:52:13 +0300

    change passwords

--- task2/assets/script/reg_form_script.js
+++ task2/assets/script/reg_form_script.js
@@ -1,13 +1,18 @@
 function checkError() {
   let email = document.getElementById("regform__email");
-  document.getElementById("regform__error").innerHTML = "";
-
   let password = document.getElementById("regform__password");
+  let yourName = document.getElementById("regform__name");
+  let yourLast = document.getElementById("regform__lastname");
+
   if (email.value == "") {
     document.getElementById("regform__error").innerHTML +=
       "Укажите адрес Gmail<br>";
   }
   if (password.value == "") {
     document.getElementById("regform__error").innerHTML += "Введите пароль<br>";
+  }
+  if (yourName.value == "" || yourLast.value == "") {
+    document.getElementById("regform__names_error").innerHTML +=
+      "Введите Ваши имя и фамилию<br>";
   }
 }
