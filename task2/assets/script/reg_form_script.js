41e22084b1e5a7426c201e174aa71b3df8d19677
Vlada <lartwoods@gmail.com>
2022-12-18 13:27:04 +0300

    test

--- task2/assets/script/reg_form_script.js
+++ task2/assets/script/reg_form_script.js
@@ -3,11 +3,14 @@ function checkError() {
   let password = document.getElementById("regform__password");
   let yourName = document.getElementById("regform__name");
   let yourLast = document.getElementById("regform__lastname");
+  let passwordConfirm = document.getElementById("regform__confirm");
   document.getElementById("regform__error").innerHTML = "";
   document.getElementById("regform__names_error").innerHTML = "";
+  document.getElementById("regform__error_gmail").innerHTML = "";
+  document.getElementById("regform__error_confirm").innerHTML = "";
 
   if (email.value == "") {
-    document.getElementById("regform__error").innerHTML +=
+    document.getElementById("regform__error_gmail").innerHTML +=
       "Укажите адрес Gmail<br>";
   }
   if (password.value == "") {
@@ -16,5 +19,13 @@ function checkError() {
   if (yourName.value == "" || yourLast.value == "") {
     document.getElementById("regform__names_error").innerHTML +=
       "Введите Ваши имя и фамилию<br>";
+  }
+  if (passwordConfirm.value == "") {
+    document.getElementById("regform__error_confirm").innerHTML +=
+      "Подтвердите пароль<br>";
+  }
+  if (passwordConfirm.value !== password.value) {
+    document.getElementById("regform__error_confirm").innerHTML +=
+      "Пароли не совпадают<br>";
   }
 }
